c95227fa8ed9b4110e4801647346ab7e794b3c26
edelveisx <edelveisx@gmail.com>
2024-04-01 18:41:42 +0500

    Fix folder name

--- sphere_backend/sphere_backend/wsgi.py
+++ sphere_backend/sphere_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sphera_backend project.
+WSGI config for sphere_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sphera_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sphere_backend.settings')
 
 application = get_wsgi_application()
