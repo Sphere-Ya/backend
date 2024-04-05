5bb0842ccc7cc7bb59754c6d5f6f3ea1e9f6cf12
Даниил <danil4k777@gmail.com>
2024-04-05 23:13:17 +0300

    feat: add model Country

--- sphere_backend/events/models.py
+++ sphere_backend/events/models.py
@@ -1,3 +1,21 @@
+"""Models for Events."""
+
 from django.db import models
 
-# Create your models here.
+
+class Country(models.Model):
+    """Model Country."""
+    name = models.CharField(max_length=50,
+                            blank=False,
+                            verbose_name="Название страны",
+                            help_text="Введите название страны",
+                            unique=True,
+                            )
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Страна"
+        verbose_name_plural = "Страны"
+
+    def __str__(self):
+        return f'{self.name}'
