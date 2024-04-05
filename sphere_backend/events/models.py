8563d02d51ee0d80efe2d9fbc9f4962359c6710d
Даниил <danil4k777@gmail.com>
2024-04-06 00:04:27 +0300

    feat: add model City

--- sphere_backend/events/models.py
+++ sphere_backend/events/models.py
@@ -1,3 +1,44 @@
+"""Models for Events."""
+
 from django.db import models
 
-# Create your models here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class City(models.Model):
+    """Model City."""
+    name = models.CharField(max_length=50,
+                            blank=False,
+                            verbose_name="Название города",
+                            help_text="Введите название города",
+                            unique=False,  # False or True. Что то переклинило
+                            )
+    country_id = models.ForeignKey(Country,
+                                   on_delete=models.CASCADE,
+                                   blank=False,
+                                   verbose_name="id страны",
+                                   related_name='cities')
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Город"
+        verbose_name_plural = "Города"
+
+    def __str__(self):
+        return f'{self.name}'
