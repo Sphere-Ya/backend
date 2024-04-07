b049d2d1e18521be414e214d452cbd340b3fe402
Даниил <danil4k777@gmail.com>
2024-04-08 01:41:17 +0300

    feat: add model Street

--- sphere_backend/events/models.py
+++ sphere_backend/events/models.py
@@ -21,24 +21,47 @@ from django.db import models
 
 
 
-class City(models.Model):
-    """Model City."""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Street(models.Model):
+    """Model Street."""
     name = models.CharField(max_length=50,
                             blank=False,
-                            verbose_name="Название города",
-                            help_text="Введите название города",
-                            unique=False,  # False or True. Что то переклинило
+                            verbose_name="Название улицы",
+                            help_text="Введите название улицы",
+                            unique=False,
                             )
-    country_id = models.ForeignKey(Country,
-                                   on_delete=models.CASCADE,
-                                   blank=False,
-                                   verbose_name="id страны",
-                                   related_name='cities')
+    city_id = models.ForeignKey(City,
+                                on_delete=models.CASCADE,
+                                blank=False,
+                                verbose_name="id города",
+                                related_name='streets')
 
     class Meta:
         ordering = ["name"]
-        verbose_name = "Город"
-        verbose_name_plural = "Города"
+        verbose_name = "Улица"
+        verbose_name_plural = "Улицы"
 
     def __str__(self):
         return f'{self.name}'
