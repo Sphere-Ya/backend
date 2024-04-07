3e9ea3b01bec06a370b25b2c6a76aeb1dbc51f83
edelveisx <edelveisx@gmail.com>
2024-04-07 21:08:07 +0500

    Модели Events, Participant, Speaker + Миграции

--- sphere_backend/events/admin.py
+++ sphere_backend/events/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 
-from .models import EventSpecialization, File, Interest
+from .models import (EventSpecialization, Event,
+                     File, Interest, Participant, Speaker)
 
 
 @admin.register(EventSpecialization)
@@ -30,4 +32,42 @@ class FileAdmin(admin.ModelAdmin):
     )
     search_fields = ('name',)
     list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+class EventMPTTModelAdmin(MPTTModelAdmin):
+    # specify pixel amount for this ModelAdmin only:
+    mptt_level_indent = 20
+    list_display = (
+        'name',
+        'started_at',
+        'ended_at',
+    )
+
+
+admin.site.register(Event, EventMPTTModelAdmin)
+
+
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'event',
+        'participation_format',
+        'status'
+    )
+    search_fields = ('user', 'event')
+    list_filter = ('user', 'event')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Speaker)
+class SpeakerAdmin(admin.ModelAdmin):
+    list_display = (
+        'participant',
+        'event',
+        'type',
+    )
+    search_fields = ('participant', 'event')
+    list_filter = ('participant', 'event')
     empty_value_display = '-пусто-'
