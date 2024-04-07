6f56a8cc1a774d621c4190d5193fe09b6c63ea15
Даниил <danil4k777@gmail.com>
2024-04-08 01:54:17 +0300

    feat: add model Building

--- sphere_backend/events/models.py
+++ sphere_backend/events/models.py
@@ -44,24 +44,54 @@ from django.db import models
 
 
 
-class Street(models.Model):
-    """Model Street."""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Building(models.Model):
+    """Model Building."""
     name = models.CharField(max_length=50,
                             blank=False,
-                            verbose_name="Название улицы",
-                            help_text="Введите название улицы",
+                            verbose_name="Название здания",
+                            help_text="Введите название здания",
                             unique=False,
                             )
-    city_id = models.ForeignKey(City,
-                                on_delete=models.CASCADE,
-                                blank=False,
-                                verbose_name="id города",
-                                related_name='streets')
+    latitude = models.FloatField(verbose_name="Широта",
+                                 help_text="Введите широту здания",
+                                 )
+    longitude = models.FloatField(verbose_name="Долгота",
+                                  help_text="Введите долготу здания",
+                                  )
+    street_id = models.ForeignKey(Street,
+                                  on_delete=models.CASCADE,
+                                  blank=False,
+                                  verbose_name="id улицы",
+                                  related_name='buildings'
+                                  )
 
     class Meta:
         ordering = ["name"]
-        verbose_name = "Улица"
-        verbose_name_plural = "Улицы"
+        verbose_name = "Здание"
+        verbose_name_plural = "Здания"
 
     def __str__(self):
         return f'{self.name}'
