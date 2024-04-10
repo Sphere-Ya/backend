c82c2665cdda8e94b96100f549d98e920bfccd30
Даниил <danil4k777@gmail.com>
2024-04-10 05:20:54 +0300

    feat: add admin models (Country, City, Street, Building)

--- sphere_backend/events/admin.py
+++ sphere_backend/events/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import EventSpecialization, File, Interest
+from .models import (
+    EventSpecialization, File, Interest, Country, City, Street, Building
+    )
 
 
 @admin.register(EventSpecialization)
@@ -25,6 +27,46 @@ class InterestAdmin(admin.ModelAdmin):
 
 @admin.register(File)
 class FileAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Country)
+class CountyAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Street)
+class StreetAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+    search_fields = ('name',)
+    list_filter = ('name',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Building)
+class BuildingAdmin(admin.ModelAdmin):
     list_display = (
         'name',
     )
