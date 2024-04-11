fc3c600e09e96b25f342951e57c50b0b34914109
edelveisx <edelveisx@gmail.com>
2024-04-11 22:29:04 +0500

    fix

--- sphere_backend/events/admin.py
+++ sphere_backend/events/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
+from mptt.admin import MPTTModelAdmin
 
-from .models import EventSpecialization, File, Interest
+from .models import (Anketa, Event, EventSpecialization, File,
+                     Interest, Participant, Speaker)
 
 
 @admin.register(EventSpecialization)
