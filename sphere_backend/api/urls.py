8246a04c00c6b79181e313f56ae284c415794b77
Даниил <danil4k777@gmail.com>
2024-04-15 14:40:35 +0300

    feat: Add endpoints for InterestView and EventSpecializationViewSet

--- sphere_backend/api/urls.py
+++ sphere_backend/api/urls.py
@@ -2,15 +2,20 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views_users import ExtensionUserViewSet
+from .views import (EventSpecializationViewSet, InterestView)
 
 app_name = 'api'
 
 router_v1 = DefaultRouter()
 router_v1.register('users', ExtensionUserViewSet)
+router_v1.register('EventSpecialization', EventSpecializationViewSet)
+router_v1.register('Interest', InterestView)
 
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
     # эндпоинты, для авторизации по токенам с помощью authtoken:
     path('auth/', include('djoser.urls.authtoken')),
+    path('eventSpecialization/', include(router_v1.urls)),
+    path('interest/', include(router_v1.urls))
 ]
