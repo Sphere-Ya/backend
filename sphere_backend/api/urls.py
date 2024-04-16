e04dad3776276acb30121fbb068bfddc4b2e1e05
edelveisx <edelveisx@gmail.com>
2024-04-16 14:57:27 +0500

    В модели анкеты поправлена связь с юзером 1:1
    В модели анкеты поправлена связь с юзером 1:1

--- sphere_backend/api/urls.py
+++ sphere_backend/api/urls.py
@@ -3,15 +3,15 @@ from rest_framework.routers import DefaultRouter
 
 from .views_users import ExtensionUserViewSet
 from .views_events import EventViewSet
-from .views import EventSpecializationViewSet, InterestView
+# from .views import EventSpecializationViewSet, InterestView
 
 app_name = 'api'
 
 router_v1 = DefaultRouter()
 router_v1.register('users', ExtensionUserViewSet)
 router_v1.register('events', EventViewSet, basename='events')
-router_v1.register('event-specialization', EventSpecializationViewSet)
-router_v1.register('interest', InterestView)
+# router_v1.register('event-specialization', EventSpecializationViewSet)
+# router_v1.register('interest', InterestView)
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
